f3f9ad176d6ea89431b93c05dcd3579d7b8bda15
Mirko Alarcon <mikos4g@gmail.com>
2022-11-13 17:12:29 -0400

    subida de proyecto backend

---
+++ src/main/java/bo/edu/ucb/sa/StrangerAccounts/StrangerAccountsApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sa.StrangerAccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrangerAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrangerAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/sa/StrangerAccounts/StrangerAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sa.StrangerAccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrangerAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
